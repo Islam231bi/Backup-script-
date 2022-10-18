741f0b5045a5b7b5fe571ce50407bd62c92cdf68
islam231bi <islambassuni@gmail.com>
2022-10-18 16:32:11 +0200

    updated the script

--- backupd.sh
+++ backupd.sh
@@ -9,6 +9,33 @@ backupdir=$2
 interval=$3
 max=$4
 
-
+# Files count in the backup dir
 cnt=$( ls $backupdir | wc -l)
+
+# Date and time information
+date=$(date +%F)
+hour=$(date +%H)
+min=$(date +%M)
+sec=$(date +%S)
+
+
+if [ $cnt -lt $max ]                                                                                                 
+then 
+  cp -R $dir $backupdir$date"-"$hour"-"$min"-"$sec
+  cnt=$((cnt+1))
+else
+  file=($backupdir/*)
+  rm -rf $file
+  cp -R $dir $backupdir$date"-"$hour"-"$min"-"$sec
+fi
+
+# Creating necessary files
+ls -lr $dir > directory-info.last
+touch  directory-info.new
+> changes_made.txt
+
+
+
+
+
 
